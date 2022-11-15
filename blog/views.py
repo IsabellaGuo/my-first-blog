7ef4248a7756fca121c71792e5a517617fcb5042
Isabella <isabella.mozart@gmail.com>
2022-11-15 11:36:50 -0800

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Post # The dot before models means current directory or current application
+from django.utils import timezone
 
 # A view is a place where we put the "logic" of our application. 
-
+# "View" supposed to connect models and templates
+# in a "View" we decide what (model) will be displayed in a template
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    # 'request' is everything we receive from the user via the Internet.
+    # the last parameter {} is a place in which we can add some things for the template to use.
+    
